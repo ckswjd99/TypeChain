4a5b7004a4a7573b0faf415b6f1093ef8d818291
ckswjd99 <ckswjd99@snu.ac.kr>
2021-02-19 23:05:40 +0900

    0.8 Blockchain Creating a Block

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+
+const genesisBlock: Block = new Block(0, "2020", "", "Hello World!", 123456);
+
+let blockchain: [Block] = [genesisBlock];
+
+console.log(blockchain);
+
+export {};
