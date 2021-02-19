75c2fdab85ebc8205134497ac72acd16974743cb
ckswjd99 <ckswjd99@snu.ac.kr>
2021-02-19 23:17:49 +0900

    0.9 Creating a Block part Two

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import * as CryptoJS from "crypto-js"
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+            index: number, previousHash:string, timestamp:number, data:string
+        ): string => CryptoJS.SHA256(
+            index + previousHash + timestamp + data
+        ).toString();
 
     constructor(
         index: number,
